7f1d99c14bc1f35a9b6571df1fa957b26e081087
Maria Idalina <mridalina@gmail.com>
2022-11-26 22:37:53 -0300

    Finalizando Javascript Básico

---
+++ JavaScript/repeticao.js
@@ -0,0 +1,29 @@
+/*for (let i=1; i<=10; i++) {
+    console.log(i);
+}
+
+for (let i=1; i<=10; i++) {
+    if (i===5) {
+        //break
+        continue
+    }
+    console.log(i)
+}
+
+
+let interator = 1
+while (interator <=20) {
+    console.log(interator)
+    
+    interator++
+}
+
+*/
+//For OF - Array
+let number = [2,4,3,5,6]
+
+for( num of number) {
+    if (num > 4) {
+        console.log(num)
+    }
+}
